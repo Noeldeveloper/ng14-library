63d18b780739534b8a0c40356b87ed0e177cf39d
Noeldeveloper <noeldesarrollo@gmail.com>
2022-07-22 12:30:26 +0200

    Initial commit

---
+++ testproject/projects/main/package2/test2.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { Test2Component } from './test2.component';
+
+@NgModule({
+  declarations: [Test2Component],
+  imports: [],
+  exports: [],
+})
+export class Test2Module {}
